19cf7012dbf091b64a4d2c354bcca15fd2efeb33
Sanchit Save <sanchitsave@gmail.com>
2025-02-26 19:05:01 +0530

    removing react routing old

--- server.js
+++ server.js
@@ -188,9 +188,9 @@ app.delete('/ball-records/:match_id/last', (req, res) => {
 });
 
 // Catch-all route for React routing
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
